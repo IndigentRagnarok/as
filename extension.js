8ca874757818aee62fa13c391dc9d51508949320
IndigentRagnarok <slipknota7x37@gmail.com>
2016-12-02 13:43:47 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -33,7 +33,7 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("/me Cunt!!!");
+              API.sendChat("/me @IndigentRagnarok is a Cunt!!!");
             }
           }
         };
